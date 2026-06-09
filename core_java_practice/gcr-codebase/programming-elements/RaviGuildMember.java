3fba43fe6ff72c7e827c5dcfb3b85cde2baf7552
tk <tktarunkushwah214@gmail.com>
2026-06-09 08:40:54 +0530

    [Tarun Kushwah].add Added a scnerio based problem

---
+++ core_java_practice/gcr-codebase/programming-elements/RaviGuildMember.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class RaviGuildMember {
+    public static void main(String[] args) {
+        String name="Ravi";
+        int age=22;
+        int rank=2;
+        long salary=50000;
+        float fee=5000;
+        int bonus=(int)(salary*12/100);
+        System.out.println("the name is:"+ name);
+        System.out.println("the age is:"+ age);
+        System.out.println("the rank is:"+ rank);
+        System.out.println("the salary is:"+ salary);
+        System.out.println("the membership fee is:"+ fee);
+        System.out.println("the bonus is:"+ bonus);
+    }
+}
